4d7e418e6d58e4230288bd024b338b1e2292c8b4
Amit Kumar <akamit21@outlook.com>
2020-05-12 14:14:09 +0530

    Revert "fuse and DnD added"
    This reverts commit 870192459858110eea9ed6b02d3b9c2b9fa2b6d9.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
-import Dashboard from "./app/components/dashboard/Dashboard";
-import { UserList, UserCreate, UserEdit } from "./app/components/users/Users";
-import { PostList, PostEdit, PostCreate } from "./app/components/posts/Posts";
+import Dashboard from "./Dashboard";
+import { UserList, UserCreate, UserEdit } from "./Users";
+import { PostList, PostEdit, PostCreate } from "./Posts";
 import PostIcon from "@material-ui/icons/Book";
 import UserIcon from "@material-ui/icons/Group";
 // import simpleRestProvider from "ra-data-simple-rest";
